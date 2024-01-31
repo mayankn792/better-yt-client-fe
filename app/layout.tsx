875764d1bd472cd039cb0c278d7c09f4231eed52
Mayank Negi <mayankn792@gmail.com>
2024-01-31 22:31:38 +0530

    add basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import '@/app/ui/global.css';
+import SideNav from '@/app/ui/dashboard/sidenav';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <div className="flex flex-row">
+      <div className="basis-1/12">01</div>
+      <div className="basis-11/12">{children}</div>
+    </div>
   );
 }
